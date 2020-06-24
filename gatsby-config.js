1ac84f30552a7d698b5409df94473506ef7ae9f9
sidtiwari98 <sidtiwari98@gmail.com>
2020-06-24 23:22:33 +0530

    used gatsby plugins for better pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-flow`]
+  plugins: [
+    `gatsby-plugin-flow`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mastermind`,
+        short_name: `mm`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        display: `browser`,
+        icon: `src/images/mastermind.png`,
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 };
